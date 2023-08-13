6c24fb29569bba74d99f1e05d42ad637b0f51346
Kaziraiyhan <112329938+Kaziraiyhan@users.noreply.github.com>
2023-08-13 21:18:45 +0600

    assignment 2 is complete

---
+++ Assignment-1.js
@@ -0,0 +1,25 @@
+// 2) javascript function returns
+
+function maxNumber(numbers) {
+  if (numbers.length === 0) {
+    return null;
+  }
+
+  let max = numbers[0];
+  for (let i = 1; i < numbers.length; i++) {
+    if (numbers[i] > max) {
+      max = numbers[i];
+    }
+  }
+
+  return max;
+}
+
+const numbers1 = [10, 20, 5, 30, 25];
+console.log(maxNumber(numbers1));
+
+const numbers2 = [5, 15, 7];
+console.log(maxNumber(numbers2));
+
+const emptyArray = [];
+console.log(maxNumber(emptyArray));
